bbd61799b33b5c138dbe55dd17f97c8882c9f64a
SolomonBekele <solubman28@gmial.com>
2022-09-21 03:51:19 -0400

    [fix] merge main ranch of nolawi--org  with local

--- Adunaz/Adunaz/asgi.py
+++ Adunaz/Adunaz/asgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 ASGI config for Adunaz project.
 
@@ -15,21 +14,3 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Adunaz.settings')
 
 application = get_asgi_application()
-=======
-"""
-ASGI config for Adunaz project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Adunaz.settings')
-
-application = get_asgi_application()
->>>>>>> a2e0f1685e363926ddf5be284c1ca01ebbbc7275
